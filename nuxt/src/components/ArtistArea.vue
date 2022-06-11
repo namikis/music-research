4fd0b0184772e6c6078078ef2930c98745373107
namikis <koudoldk@icloud.com>
2022-06-12 00:52:01 +0900

    Artists API

--- nuxt/src/components/ArtistArea.vue
+++ nuxt/src/components/ArtistArea.vue
@@ -5,8 +5,8 @@
                 @searchArtist="searchArtist"
             />
             <div class="artist_items">
-                <div class="artist_item" v-for="artist_name in artist_name_list" :key="artist_name">
-                    <span @click="selectedArtist(artist_name)">{{ artist_name }}</span>
+                <div class="artist_item" v-for="artist_name in artist_name_list" :key="artist_name.artist_name">
+                    <span @click="selectedArtist(artist_name.artist_name)">{{ artist_name.artist_name }}</span>
                 </div>
             </div>
         </div>
@@ -15,6 +15,10 @@
 </template>
 <script lang="ts">
 import Vue from 'vue'
+import axios from 'axios'
+type Artists = {
+    artist_name: String
+}
 export default Vue.extend({
     props: {
         selected_artist_name:{
@@ -24,28 +28,31 @@ export default Vue.extend({
     },
     data(){
         return{
-            artist_name_list: [] as Array<string>,
-            clone_artist_name_list: [] as Array<string>,
+            artist_name_list: [] as Array<Artists>,
+            clone_artist_name_list: [] as Array<Artists>,
         }
     },
     mounted(){
-        this.artist_name_list = [
-            "ZUTOMAYO",
-            "artist2",
-            "artist3"
-        ]
-        this.clone_artist_name_list = this.artist_name_list.slice()
+        this.getAllAtristsName()
     },
     methods: {
-        selectedArtist(artist_name: string){
+        selectedArtist(artist_name: String){
             this.$emit("update:selected_artist_name", artist_name)
         },
-        searchArtist(search_name: string){
+        searchArtist(search_name: String){
+            console.log(this.clone_artist_name_list)
             this.artist_name_list = this.clone_artist_name_list.slice()
             // TODO:SpotifyAPIにsearchして日本語のアーティスト名を取得する
             if(search_name !== ""){
-                this.artist_name_list = this.artist_name_list.filter(artist_name => artist_name == search_name)
+                this.artist_name_list = this.artist_name_list.filter(artist_name => artist_name.artist_name == search_name)
             }
+        },
+        getAllAtristsName(){
+            const url = "http://localhost:5000/artists"
+            axios.get(url).then((response) => {
+                this.artist_name_list = response.data
+                this.clone_artist_name_list = this.artist_name_list.slice()
+            })
         }
     }
 })
@@ -56,6 +63,7 @@ export default Vue.extend({
         background: lightblue;
         height:100vh;
         width: 30%;
+        overflow: scroll;
     }
     .artist_field{
         width: 85%;
